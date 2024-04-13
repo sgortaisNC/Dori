935ebd9b2168e57b3d4999655c7292c208047956
Sébastien Gortais <sgortais@net-com.fr>
2024-04-13 11:33:21 +0200

    :sparkles: Add PWA

--- src/app/page.js
+++ src/app/page.js
@@ -29,14 +29,7 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <h1>DORI</h1>
       </div>
 
       <div className={styles.grid}>
